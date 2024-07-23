e33e903df6c129bedba7086ae7eb3f898935bbbf
Shivam Roy <devopsroy9@gmail.com>
2024-07-23 23:23:01 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from "@astrojs/tailwind";
 
-import amplify from 'astro-aws-amplify';
+import awsamplifyamplify from 'astro-aws-amplify';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'server',
-  adapter: amplify()
+  adapter: awsamplify(),
 });
